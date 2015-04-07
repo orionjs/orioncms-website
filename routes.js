578a0b0f29450b9c995e7afc5c7e1cff7b84728b
Nicolás López <nicolaslopezj@me.com>
2015-04-07 17:32:32 -0300

    changed to new repository

--- routes.js
+++ routes.js
@@ -11,47 +11,47 @@ Router.route('/', function() {
 }, { name: 'home' });
 
 Router.route('/docs', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/README.md');
 }, { name: 'intro' });
 
 Router.route('/docs/entities', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/entities/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/entities/README.md');
 }, { name: 'entities' });
 
 Router.route('/docs/dictionary', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/dictionary/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/dictionary/README.md');
 }, { name: 'dictionary' });
 
 Router.route('/docs/attributes', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/attributes/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/attributes/README.md');
 }, { name: 'attributes' });
 
 Router.route('/docs/users', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/users/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/users/README.md');
 }, { name: 'users' });
 
 Router.route('/docs/configuration', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/config/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/config/README.md');
 }, { name: 'configuration' });
 
 Router.route('/docs/languages', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/languages/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/languages/README.md');
 }, { name: 'languages' });
 
 Router.route('/docs/api', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/core/master/api/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/core/api/README.md');
 }, { name: 'api' });
 
 Router.route('/docs/pages', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/pages/master/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/pages/README.md');
 }, { name: 'pages' });
 
 Router.route('/docs/relationships', function() {
 	var router = this;
 	router.layout('layout');
 	router.render('loading');
-	Meteor.call('getReadme', 'https://raw.githubusercontent.com/orionjs/relationships/master/README.md', function(error, result1) {
-		Meteor.call('getReadme', 'https://raw.githubusercontent.com/orionjs/users-attribute/master/README.md', function(error, result2) {
+	Meteor.call('getReadme', 'https://raw.githubusercontent.com/orionjs/orion/master/relationships/README.md', function(error, result1) {
+		Meteor.call('getReadme', 'https://raw.githubusercontent.com/orionjs/orion/master/users-attribute/README.md', function(error, result2) {
 			var result = result1 + result2;
 			router.render('content', {data: {content: result}});
 		})
@@ -59,13 +59,13 @@ Router.route('/docs/relationships', function() {
 }, { name: 'relationships' });
 
 Router.route('/docs/filesystem', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/filesystem/master/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/filesystem/README.md');
 }, { name: 'filesystem' });
 
 Router.route('/docs/file-attribute', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/file-attribute/master/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/file-attribute/README.md');
 }, { name: 'file_attribute' });
 
 Router.route('/docs/text-editors', function() {
-	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/froala-editor/master/README.md');
+	doRouteAction(this, 'https://raw.githubusercontent.com/orionjs/orion/master/froala-editor/README.md');
 }, { name: 'text_editors' });
