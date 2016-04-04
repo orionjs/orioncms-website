d18db2f6ac259388fb2b4e1db5cc0b01dd10f3e0
Nicolás López <nicolaslopezj@me.com>
2016-04-04 19:46:19 -0300

    update to meteor 1.3

--- client/views/about/about.js
+++ client/views/about/about.js
@@ -1,44 +1,48 @@
-Template.about.onRendered(function() {
+Template.about.onRendered(function () {
   $('ul.tabs').tabs();
-  HTTP.get('https://api.github.com/repos/orionjs/orion/stats/contributors', function(error, response) {
+  HTTP.get('https://api.github.com/repos/orionjs/orion/stats/contributors', function (error, response) {
     if (!error) {
       Session.set('contributors_core', response.data.reverse());
     }
   });
 
-  HTTP.get('https://api.github.com/repos/orionjs/tutorials/stats/contributors', function(error, response) {
+  HTTP.get('https://api.github.com/repos/orionjs/tutorials/stats/contributors', function (error, response) {
     if (!error) {
       Session.set('contributors_tutorials', response.data.reverse());
     }
   });
 
-  HTTP.get('https://api.github.com/repos/orionjs/examples/stats/contributors', function(error, response) {
+  HTTP.get('https://api.github.com/repos/orionjs/examples/stats/contributors', function (error, response) {
     if (!error) {
       Session.set('contributors_examples', response.data.reverse());
     }
   });
-})
+});
 
 Template.about.helpers({
   gravatarFor: function (email) {
     return 'http://www.gravatar.com/avatar/' + md5(email) + '?s=500';
   },
-  coreContribution: function() {
+
+  coreContribution: function () {
     return Session.get('contributors_core');
   },
-  docsContribution: function() {
+
+  docsContribution: function () {
     return Session.get('contributors_docs');
   },
-  tutorialsContribution: function() {
+
+  tutorialsContribution: function () {
     return Session.get('contributors_tutorials');
   },
-  examplesContribution: function() {
+
+  examplesContribution: function () {
     return Session.get('contributors_examples');
-  }
+  },
 });
 
 Template.contribution.helpers({
-  format: function(number) {
+  format: function (number) {
     number += '';
     x = number.split('.');
     x1 = x[0];
@@ -47,23 +51,29 @@ Template.contribution.helpers({
     while (rgx.test(x1)) {
       x1 = x1.replace(rgx, '$1' + ',' + '$2');
     }
+
     return x1 + x2;
   },
-  getCommits: function() {
+
+  getCommits: function () {
     return this.total == 1 ? '1 commit' : this.total + ' commits';
   },
-  getAdditions: function() {
+
+  getAdditions: function () {
     var count = 0;
-    _.each(this.weeks, function(week){
+    _.each(this.weeks, function (week) {
       count += week.a;
     });
+
     return count;
   },
-  getDeletes: function() {
+
+  getDeletes: function () {
     var count = 0;
-    _.each(this.weeks, function(week){
+    _.each(this.weeks, function (week) {
       count += week.d;
     });
+
     return count;
-  }
+  },
 });
